c74601d51b0074abd3082268001e2f3d31ed4e00
kimdong2404 <lkdong2404@gmail.com>
2025-01-24 10:01:46 +0700

    Create Animation
    Create animation to Hero Enemy

---
+++ Assets/Bum/Defens-game/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator m_anim;
+    private void Awake()
+    {
+        m_anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (m_anim)
+            {
+                m_anim.SetBool(Const.ATTACK_ANIM,true);
+            }
+
+        }
+        
+    }
+
+}
