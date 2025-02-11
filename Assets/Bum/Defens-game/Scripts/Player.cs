073276d531c9e20f7fe41e25872de4431c625ad1
kimdong2404 <lkdong2404@gmail.com>
2025-02-11 09:54:01 +0700

    Add const and player script
    Add const and player script

--- Assets/Bum/Defens-game/Scripts/Player.cs
+++ Assets/Bum/Defens-game/Scripts/Player.cs
@@ -2,31 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace Bum.Demogame
 {
-    private Animator m_anim;
-    private void Awake()
+    public class Player : MonoBehaviour
     {
-        m_anim = GetComponent<Animator>();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
+        public float atkRate;
+        private Animator m_anim;
+        private float m_curAtkRate;
+        private bool m_IsAttacked;
+        private void Awake()
         {
-            if (m_anim)
+            m_anim = GetComponent<Animator>();
+            m_curAtkRate = atkRate;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
             {
-                m_anim.SetBool(Const.ATTACK_ANIM,true);
+                if (m_anim)
+                {
+                    m_anim.SetBool(Const.ATTACK_ANIM, true);
+                    m_IsAttacked = true;
+                }
+                if (m_IsAttacked)
+                {
+                    m_curAtkRate = Time.deltaTime;
+                    if (m_curAtkRate <= 0)
+                    {
+                        m_IsAttacked = false;
+                        m_curAtkRate = atkRate;
+                    }
+
+
+
+                }
+
             }
 
         }
-        
+        public void ResetatkAnim()
+        {
+            if (m_anim)
+                m_anim.SetBool(Const.ATTACK_ANIM, false);
+        }
     }
 
-}
+
+}       
