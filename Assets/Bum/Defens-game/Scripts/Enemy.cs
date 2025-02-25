56ffb32186e99b8bd98ee4f31f284c94a8902616
kimdong2404 <lkdong2404@gmail.com>
2025-02-25 10:10:05 +0700

    Add random coinbonus
    Add random coinbonus when kill enemy

--- Assets/Bum/Defens-game/Scripts/Enemy.cs
+++ Assets/Bum/Defens-game/Scripts/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Bum.Demogame
 {
-    public class Enemy : MonoBehaviour
+    public class Enemy : MonoBehaviour, icomponentchecking
     {
         private Animator m_anim;
         private Rigidbody2D m_rb;
@@ -12,55 +12,59 @@ namespace Bum.Demogame
         public float speed;
         public float atkDistance;
         private bool m_IsDead;
+        public int minCoinBonus;
+        public int maxCoinBonus;
 
         private GameManager m_gm;
              private void Awake()
-        {
-            m_anim = GetComponent<Animator>();
-            m_rb = GetComponent<Rigidbody2D>();
-            m_player=FindObjectOfType<Player>();
-            m_gm =FindObjectOfType<GameManager>();
+             {
+                m_anim = GetComponent<Animator>();
+                m_rb = GetComponent<Rigidbody2D>();
+                m_player=FindObjectOfType<Player>();
+                m_gm =FindObjectOfType<GameManager>();
 
-        }
+             }
         // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-        public bool Iscomponentsnull() 
-        {
-            return m_anim==null|| m_rb==null|| m_player==null;
-        }
-        // Update is called once per frame
-        void Update()
-        {
-            if (Iscomponentsnull()) return;
-            float DistToPlayer = Vector2.Distance(m_player.transform.position, transform.position);
-            if (DistToPlayer <= atkDistance)
+            void Start()
             {
-                m_anim.SetBool(Const.ATTACK_ANIM, true);
+
+            }
+            public bool Iscomponentsnull() 
+            {
+                return m_anim == null || m_rb == null || m_player == null || m_gm == null;
+            }
+            // Update is called once per frame
+            void Update()
+            {
+                if (Iscomponentsnull()) return;
+                float DistToPlayer = Vector2.Distance(m_player.transform.position, transform.position);
+                if (DistToPlayer <= atkDistance)
+                {
+                    m_anim.SetBool(Const.ATTACK_ANIM, true);
+                    m_rb.velocity = Vector2.zero;
+                }
+                else
+                {
+                    m_rb.velocity = new Vector2(-speed, m_rb.velocity.y);
+                }
+
+            }
+            public void Die()
+            {
+                if (Iscomponentsnull()|| m_IsDead) return;
+                m_IsDead = true;
+                m_anim.SetTrigger(Const.DEAD_ANIM); 
                 m_rb.velocity = Vector2.zero;
-            }
-            else
-            {
-                m_rb.velocity = new Vector2(-speed, m_rb.velocity.y);
-            }
-
-        }
-        public void Die()
-        {
-            if (Iscomponentsnull()|| m_IsDead) return;
-            m_IsDead = true;
-            m_anim.SetTrigger(Const.DEAD_ANIM); 
-            m_rb.velocity = Vector2.zero;
-            gameObject.layer = LayerMask.NameToLayer(Const.DEAD_ANIM);
-            if (m_gm)
+                gameObject.layer = LayerMask.NameToLayer(Const.DEAD_ANIM);
                 m_gm.Score++;
+                int CoinBonus = Random.Range(minCoinBonus, maxCoinBonus);
+                Debug.Log(CoinBonus);
+                Pref.coins += CoinBonus;
 
-            Destroy(gameObject,2f);
+                Destroy(gameObject,2f); 
             
            
-        }
+            }
 
     }
 }
