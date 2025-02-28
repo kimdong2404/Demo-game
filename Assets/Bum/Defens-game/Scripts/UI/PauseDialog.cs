d4c13494c1d06a93620dceb5c882d833b9400c3b
kimdong2404 <lkdong2404@gmail.com>
2025-02-28 11:05:40 +0700

    Create Script PauseDialog

--- Assets/Bum/Defens-game/Scripts/UI/PauseDialog.cs
+++ Assets/Bum/Defens-game/Scripts/UI/PauseDialog.cs
@@ -1,11 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Bum.Demogame
 {
     public class PauseDialog : Dialog
     {
-
+        public override void Show(bool isShow)
+        {
+            Time.timeScale = 0f;
+            base.Show(isShow);
+        }
+        public void Resume()
+        {
+            Close();
+        }
+        public void Replay()
+        {
+            Close();
+            SceneManager.LoadScene(Const.GAMEPLAY_SCENE);
+        }
+        public override void Close()
+        {
+            Time.timeScale = 1f;
+            base.Close();
+        }
     }
 }
