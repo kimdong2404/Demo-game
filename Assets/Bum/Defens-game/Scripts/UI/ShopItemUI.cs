0655276ddaf4b5edec5ba8e5949e94a829c48314
kimdong2404 <lkdong2404@gmail.com>
2025-03-03 11:53:05 +0700

    Create ShopItemUI Scrip
    Tạo các thông số cho một sản phẩm trên shop

---
+++ Assets/Bum/Defens-game/Scripts/UI/ShopItemUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Bum.Demogame
+{
+    public class ShopItemUI : MonoBehaviour
+    {
+        public Text pricetxt;
+        public Image hud;
+        public Button btn;
+
+        public void UpdateUI(ShopItem item, int itemIdx)
+        {
+            if (item == null) return;
+
+            if (hud)
+                hud.sprite = item.previewImg;//đa khai bao cai nay trong Datastruct
+            bool isUnclocked = Pref.GetBool(Const.PLAYER_PREFIX_PREF + itemIdx);
+
+            if (isUnclocked)
+            {
+                if (Pref.curPlayerId == itemIdx)
+                {
+                    if (pricetxt)
+                        pricetxt.text = "Acive";
+                }
+                else if(pricetxt)
+                {
+                    pricetxt.text = "Owned";
+                }
+            }
+            else
+            {
+                if (pricetxt)
+                    pricetxt.text = item.price.ToString();
+            }
+        }
+    }
+
+
+}
